ea1aa9e3af634a9abdb2c0f0e1f175cedf50ad37
Irvin Reyes <reyes.yerp@gmail.com>
2020-06-30 11:19:41 -0500

    Add missing cases

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 module.exports = (num) => {
   if (num % 15 === 0) return "FizzBuzz";
+  if (num % 3 === 0) return "Fizz";
+  if (num % 5 === 0) return "Buzz";
   return `${num}`;
 };
--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -5,6 +5,15 @@ describe("fizzBuzz()", () => {
     expect(fizzBuzz(15)).toBe("FizzBuzz");
     expect(fizzBuzz(30)).toBe("FizzBuzz");
   });
+  it('returns "Fizz" for multiples of 3', () => {
+    expect(fizzBuzz(3)).toBe("Fizz");
+    expect(fizzBuzz(33)).toBe("Fizz");
+  });
+
+  it('returns "Buzz" for multiples of 5', () => {
+    expect(fizzBuzz(5)).toBe("Buzz");
+    expect(fizzBuzz(20)).toBe("Buzz");
+  });
   it("returns the given number for multiples of neither 3 nor 5", () => {
     expect(fizzBuzz(1)).toBe("1");
     expect(fizzBuzz(22)).toBe("22");
